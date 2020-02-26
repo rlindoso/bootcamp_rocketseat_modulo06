693a1326a7388dc4296f349e0253bd62d3a82b6d
Rafael Lindoso <rafael.lindoso@gmail.com>
2020-02-26 18:01:36 -0300

    Listando favoritos + onEndReached

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-//import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -14,6 +25,7 @@ export default class User extends Component {
     super(props);
     this.state = {
       stars: [],
+      page: 1,
     };
   }
 
@@ -21,16 +33,51 @@ export default class User extends Component {
     const { navigation } = this.props;
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred`);
+    const response = await api.get(`/users/${user.login}/starred?page=1`);
 
     this.setState({ stars: response.data });
   }
 
+  async handleEndReached() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+    const { stars, page } = this.state;
+    const response = await api.get(
+      `/users/${user.login}/starred?page=${page + 1}`
+    );
+
+    this.setState({ stars: [...stars, ...response.data], page: page + 1 });
+  }
+
   render() {
     const { stars } = this.state;
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
 
-    console.tron.log(stars);
-    return <View />;
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id || 0)}
+          onEndReached={() => this.handleEndReached()}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
 
